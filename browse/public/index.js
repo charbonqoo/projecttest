deb16d37d4a3c5269385726869f74b6eb5116ebd
charbonqoo <154399929+charbonqoo@users.noreply.github.com>
2025-06-05 11:53:55 +0900

    Add files via upload

---
+++ browse/public/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(path.join(__dirname, "public"))); // 静的ファイル（JS/CSSなど）
+
+let dataString = "";
+
+// HTMLをそのまま返す（テンプレート使わない）
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post('/', (req, res) => {
+  dataString = req.body.data;
+  // データを保存するだけ。リダイレクトでも可
+  res.redirect('/');
+});
+
+app.listen(process.env.PORT || PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
